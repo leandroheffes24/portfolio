72e00c2791b75eb7112d92faa924923b1ec82be6
Leandro Heffes <leandroheffes@outlook.es>
2024-04-14 18:16:22 -0300

    mobile navbar complete

--- src/components/Header/components/Navbar/index.jsx
+++ src/components/Header/components/Navbar/index.jsx
@@ -1,15 +1,36 @@
 import styles from './Navbar.module.css'
+import whiteLogo from '../../../../assets/images/leandro-heffes-white-logo.png'
+import { useRef, useState } from 'react'
 
 const Navbar = () => {
+    const [showNavbarList, setShowNavbarList] = useState(false)
+    const [navbarMenuIconClass, setNavbarMenuIconClass] = useState('fa-solid fa-bars')
+    const navbarList = useRef()
+
+    const handleShowNavbarList = () => {
+        setShowNavbarList(!showNavbarList)
+        setNavbarMenuIconClass(navbarMenuIconClass === 'fa-solid fa-bars' ? 'fa-solid fa-xmark' : 'fa-solid fa-bars')
+    }
+
     return (
         <nav>
-            <div className={styles.titleContainer}>
-                <h1><a className={styles.titleLink} href="/">Leandro Heffes</a></h1>
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src={whiteLogo} alt="Leandro Heffes white logo" />
             </div>
 
             <div className={styles.openNavbarMenuIconContainer}>
-                <i className="fa-regular fa-bars"></i>
+                <i onClick={handleShowNavbarList} style={{color: "rgb(250, 250, 250)", fontSize: "1.5rem"}} className={navbarMenuIconClass}></i>
             </div>
+
+            {showNavbarList && (
+                <ul ref={navbarList} className={styles.navbarList}>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About me</a></li>
+                    <li><a href="/">What I'm doing</a></li>
+                    <li><a href="/">Proyects</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
+            )}
         </nav>
     )
 }
