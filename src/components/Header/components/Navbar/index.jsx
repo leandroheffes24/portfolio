6ba96f5d8f3d84ad40c02046ad259a1c27c0b8a3
Leandro Heffes <leandroheffes@outlook.es>
2024-04-15 10:30:48 -0300

    navbar complete

--- src/components/Header/components/Navbar/index.jsx
+++ src/components/Header/components/Navbar/index.jsx
@@ -1,8 +1,9 @@
 import styles from './Navbar.module.css'
 import whiteLogo from '../../../../assets/images/leandro-heffes-white-logo.png'
+import blackLogo from '../../../../assets/images/leandro-heffes-black-logo.png'
 import { useRef, useState } from 'react'
 
-const Navbar = () => {
+const Navbar = ({scrolled}) => {
     const [showNavbarList, setShowNavbarList] = useState(false)
     const [navbarMenuIconClass, setNavbarMenuIconClass] = useState('fa-solid fa-bars')
     const navbarList = useRef()
@@ -15,29 +16,29 @@ const Navbar = () => {
     return (
         <nav>
             <div className={styles.logoContainer}>
-                <img className={styles.logo} src={whiteLogo} alt="Leandro Heffes white logo" />
+                <img className={styles.logo} src={scrolled === true ? blackLogo : whiteLogo} alt="Leandro Heffes white logo" />
             </div>
 
             <div className={styles.openNavbarMenuIconContainer}>
-                <i onClick={handleShowNavbarList} className={navbarMenuIconClass} id={styles.openNavbarMenuIcon}></i>
+                <i onClick={handleShowNavbarList} className={scrolled === true ? `${navbarMenuIconClass} ${styles.scrolled} ${styles.openNavbarMenuIcon}` : `${navbarMenuIconClass} ${styles.openNavbarMenuIcon}`}></i>
             </div>
 
             {showNavbarList && (
                 <ul ref={navbarList} className={styles.navbarList}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About me</a></li>
-                    <li><a href="/">What I'm doing</a></li>
-                    <li><a href="/">Proyects</a></li>
-                    <li><a href="/">Contact</a></li>
+                    <li><a className={scrolled === true ? styles.scrolled : ''} href="/">Home</a></li>
+                    <li><a className={scrolled === true ? styles.scrolled : ''} href="/">About me</a></li>
+                    <li><a className={scrolled === true ? styles.scrolled : ''} href="/">What I'm doing</a></li>
+                    <li><a className={scrolled === true ? styles.scrolled : ''} href="/">Proyects</a></li>
+                    <li><a className={scrolled === true ? styles.scrolled : ''} href="/">Contact</a></li>
                 </ul>
             )}
 
             <ul className={styles.desktopNavbarList}>
-                <li><a className={styles.desktopNavbarLink} href="/">Home</a></li>
-                <li><a className={styles.desktopNavbarLink} href="/">About me</a></li>
-                <li><a className={styles.desktopNavbarLink} href="/">What I'm doing</a></li>
-                <li><a className={styles.desktopNavbarLink} href="/">Proyects</a></li>
-                <li><a className={styles.desktopNavbarLink} href="/">Contact</a></li>
+                <li><a className={scrolled === true ? `${styles.desktopNavbarLink} ${styles.scrolled}` : styles.desktopNavbarLink} href="/">Home</a></li>
+                <li><a className={scrolled === true ? `${styles.desktopNavbarLink} ${styles.scrolled}` : styles.desktopNavbarLink} href="/">About me</a></li>
+                <li><a className={scrolled === true ? `${styles.desktopNavbarLink} ${styles.scrolled}` : styles.desktopNavbarLink} href="/">What I'm doing</a></li>
+                <li><a className={scrolled === true ? `${styles.desktopNavbarLink} ${styles.scrolled}` : styles.desktopNavbarLink} href="/">Proyects</a></li>
+                <li><a className={scrolled === true ? `${styles.desktopNavbarLink} ${styles.scrolled}` : styles.desktopNavbarLink} href="/">Contact</a></li>
             </ul>
         </nav>
     )
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,31 @@
-import './Header.module.css'
+import { useState, useEffect } from 'react'
+import styles from './Header.module.css'
 import Navbar from './components/Navbar'
 
 const Header = () => {
+
+    const [scrolling, setScrolling] = useState(false)
+    console.log(scrolling);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 0){
+                setScrolling(true)
+            } else {
+                setScrolling(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
-        <header>
-            <Navbar/>
+        <header className={scrolling === true ? styles.scrolled : ''}>
+            <Navbar scrolled={scrolling}/>
         </header>
     )
 }
